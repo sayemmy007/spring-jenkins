01373b17e6a70d92e6e9edb0b345fd13568021e3
sayemmy007 <sayemmy007@gmail.com>
2023-11-30 11:10:43 +0530

    second commit

--- src/test/java/com/example/demo/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/demo/SpringJenkinsApplicationTests.java
@@ -18,6 +18,7 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
+		logger.info("Test case executing 2nd statment...");
 		assertEquals(true, true);
 	}
 
